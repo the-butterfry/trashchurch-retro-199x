3a7772a0c123aa455b697cf4b8309cc41eb4ecf9
the-butterfry <butterfry@gmail.com>
2025-09-11 19:25:05 -0700

    css changes

--- trashchurch-retro-199x/header-title-wrap-by-id.php
+++ trashchurch-retro-199x/header-title-wrap-by-id.php
@@ -5,6 +5,8 @@ $right_id = 146; // right-racoon attachment ID
 
 $left_src  = $left_id  ? wp_get_attachment_image_url( $left_id, 'medium' )  : '';
 $right_src = $right_id ? wp_get_attachment_image_url( $right_id, 'medium' ) : '';
+
+$tagline = get_bloginfo( 'description', 'display' );
 ?>
 <div class="tr-title-wrap">
   <?php if ( $left_src ) : ?>
@@ -17,9 +19,14 @@ $right_src = $right_id ? wp_get_attachment_image_url( $right_id, 'medium' ) : ''
     />
   <?php endif; ?>
 
-  <h1 class="tr-title">
-    <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-  </h1>
+  <div class="tr-title-stack">
+    <h1 class="tr-title">
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+    </h1>
+    <?php if ( $tagline ) : ?>
+      <p class="tr-tagline"><?php echo esc_html( $tagline ); ?></p>
+    <?php endif; ?>
+  </div>
 
   <?php if ( $right_src ) : ?>
     <img
